67dda01a6bd8661c9c2609422d608d62e77594e7
duke-jindu <dukechukwujindu71@gmail.com>
2023-03-29 14:04:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//1. Write an expression that checks whether an integer is odd or even.//
+
+
+//passing a intruction message to the console//
+
+Console.WriteLine("Please enter a number");
+
+//passing a console readline//
+int figure = int.Parse(Console.ReadLine());
+
+//using if else//
+if (figure % 2 == 0)
+{
+    Console.WriteLine("{0} is an even number", figure);
+}
+else
+{
+    Console.WriteLine("{0} is an ODD number", figure);
+}
+//bool even = figure % 2 == 0 ? true : false;//
